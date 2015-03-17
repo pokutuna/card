f4f6fa4ce2ed1e81d4c168c4e10838432175156d
pokutuna <popopopopokutuna@gmail.com>
2015-03-17 22:33:23 +0900

    simple

--- build/main.js
+++ build/main.js
@@ -1,1 +1,34 @@
-console.log('card');
+/// <reference path="../DefinitelyTyped/vue/vue.d.ts" />
+var App = (function () {
+    function App() {
+        console.log('hoge');
+        var cardData = this.constructCardData();
+        new Vue({
+            el: '#card-container',
+            data: {
+                cards: cardData
+            }
+        });
+    }
+    App.prototype.constructCardData = function () {
+        var cards = App.defaultCards;
+        return cards.map(function (c) {
+            return {
+                text: c,
+                isNumber: !isNaN(parseInt(c)),
+                selected: false
+            };
+        });
+    };
+    App.defaultCards = [
+        '1',
+        '2',
+        '3',
+        '5',
+        '8',
+        '13',
+        '∞'
+    ];
+    return App;
+})();
+document.addEventListener('DOMContentLoaded', function () { return new App(); });
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,37 @@
-console.log('card');
+/// <reference path="../DefinitelyTyped/vue/vue.d.ts" />
+
+interface CardData {
+    text: string;
+    isNumber: boolean;
+    selected: boolean;
+}
+
+class App {
+    static defaultCards: Array<string> = [
+        '1', '2', '3', '5', '8', '13', '∞'
+    ];
+
+    constructor() {
+        console.log('hoge')
+        var cardData = this.constructCardData();
+        new Vue({
+            el: '#card-container',
+            data: {
+                cards: cardData
+            },
+        });
+    }
+
+    constructCardData(): Array<CardData> {
+        var cards: Array<string> = App.defaultCards;
+        return cards.map(function(c: string): CardData {
+            return {
+                text: c,
+                isNumber: !isNaN(parseInt(c)),
+                selected: false,
+            }
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => new App());
